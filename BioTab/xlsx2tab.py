57d0e0b4d5dd56126c9772c2bc1ec8fe08fddf69
xie186 <xie186@purdue.edu>
2018-04-05 15:01:16 +0000

    Update biotab

--- BioTab/xlsx2tab.py
+++ BioTab/xlsx2tab.py
@@ -6,6 +6,7 @@ from os.path import basename
 class xlsx2tab:
     def __init__ (self):
         pass
+
     def output_tab(self, options):
         wb = load_workbook(options.input) 
         if not options.prefix:
@@ -14,10 +15,10 @@ class xlsx2tab:
         for sheetname in sheetnames:
             sn_slug = slugify(sheetname)            
             output_file = '{}.{}.{}'.format(options.prefix, sn_slug, "txt")
-            with open(output, "w") as output_file:
+            with open(output_file, "w") as output_handle:
                 sheet = wb[sheetname]
                 for row in sheet.iter_rows():
                     ele = []
                     for cell in row:
                         ele.append(str(cell.value))
-                    output_file.write("\t".join(ele))
+                    output_handle.write("\t".join(ele) + "\n")
