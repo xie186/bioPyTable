2df94d77e50e191991b653e1707f06ccae1215fb
Shaojun Xie <xie186@purdue.edu>
2018-07-16 10:31:05 -0400

    Update biotab BioTab/tabfilter.py

--- BioTab/tabfilter.py
+++ BioTab/tabfilter.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 
-class tab2xlsx:
+class tabfilter:
     def __init__ (self):
         pass
 
     def output_filter(self, options):
         dict_fil = {}
-        with open(options.tabfilter, 'r') as tabfil:
+        with open(options.tabfil, 'r') as tabfil:
             for line in tabfil:
                 line = line.rstrip("\n")
                 ele = line.split("\t")
-                col_index = options.column
-                if not line.startswith("#")
-                    dict_fil[ele[col_index]] ++
-
+                col_index = options.column - 1
+                if not line.startswith("#"):
+                    dict_fil[ele[col_index]] = 1
+        print(dict_fil)
         with open(options.tab, 'r') as tab, open(options.output, 'w') as output: 
             header = next(tab)
             output.write(header)
             for line in tab:
                 line = line.rstrip("\n")
                 ele = line.split("\t")
-                col_index = options.target_column
+                col_index = options.target_column -1
                 if ele[col_index] in dict_fil:
                     output.write(line + "\n")
                     
